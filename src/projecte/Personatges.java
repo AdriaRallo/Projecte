9491419d4700f9eee170fc322253d50e8f1d9dad
rallito <rallito@Rallito>
2018-03-20 09:41:29 +0100

    Aixo ja va a 2 de fondo artistee!

---
+++ src/projecte/Personatges.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecte;
+
+/**
+ *
+ * @author rallito
+ */
+public class Personatges {
+
+    private String nom;
+    private String sexe;
+    private String raça;
+    private String tecniques;
+    private String habilitats;
+    private String transformacions;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getHabilitats() {
+        return habilitats;
+    }
+
+    public void setHabilitats(String habilitats) {
+        this.habilitats = habilitats;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public String getRaça() {
+        return raça;
+    }
+
+    public void setRaça(String raça) {
+        this.raça = raça;
+    }
+
+    public String getTecniques() {
+        return tecniques;
+    }
+
+    public void setTecniques(String tecniques) {
+        this.tecniques = tecniques;
+    }
+
+    public String getTransformacions() {
+        return transformacions;
+    }
+
+    public void setTransformacions(String transformacions) {
+        this.transformacions = transformacions;
+    }
+
+}
