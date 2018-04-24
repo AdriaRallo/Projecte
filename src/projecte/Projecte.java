c3c2676de460515e67223def7f68758cb77193cc
rallito <rallito@Rallito>
2018-04-24 09:46:28 +0200

    Entrega UF2

--- src/projecte/Projecte.java
+++ src/projecte/Projecte.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  * @author rallito
  */
 public class Projecte {
-
+    
     private static final int MAX_PERSONATGES = 5;
     private static Personatges[] array = new Personatges[MAX_PERSONATGES];
     public static int opcio;
@@ -21,64 +21,112 @@ public class Projecte {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        inicialitzarVariables;
+        inicialitzarVariables();
         do {
             demanarOpcio();
             tractarOpcio();
-
+            
         } while (!opcioFinal());
-
+        
     }
-
+    
     public static void inicialitzarVariables() {
         for (int i = 0; i < array.length; i++) {
             array[i] = new Personatges();
             array[i].setOmplit(false);
-
+            
         }
     }
-
+    
     public static void demanarOpcio() {
         Scanner ent = new Scanner(System.in);
+        
         System.out.println("1. Introduir Personatge.");
         System.out.println("2. Modificar Personatge.");
         System.out.println("3. Borrar Personatge.");
         System.out.println("4. Llistar tots els Personatges.");
         System.out.println("Sortir de la App.");
         opcio = ent.skip("[\r\n]*").nextInt();
-
+        
     }
-
+    
     public static void tractarOpcio() {
-
+        
         switch (opcio) {
-
+            
             case 0:
                 System.out.println("");
                 break;
-
+            
             case 1:
                 introduirPersonatge();
                 break;
-
+            
             case 2:
                 modificarPersonatge();
                 break;
-
+            
             case 3:
                 borrarPersonatge();
                 break;
-
+            
             case 4:
                 llistarPersonatges();
                 break;
-
+            
             default:
                 System.out.println("\nOpció Incorrecta!");
         }
     }
-
+    
     public static boolean opcioFinal() {
         return opcio == 0;
     }
+    
+    public static void introduirPersonatge() {
+        
+        Scanner ent = new Scanner(System.in);
+
+        // Primer recorrem l'array fins trobar una casella no omplida o arribar al seu final
+        int i;
+        for (i = 0; i < array.length && array[i].isOmplit(); i++);
+
+        // Si no hem arribat al final és per que hem trobat una casella buida (no omplida)
+        if (i < array.length) {
+            System.out.println("\nNom:");
+            array[i].setNom(ent.skip("[\r\n]*").nextLine());
+            
+            System.out.println("Raça:");
+            array[i].setRaça(ent.skip("[\r\n]*").nextLine());
+            
+            System.out.println("Familia:");
+            array[i].setFamilia(ent.skip("[\r\n]*").nextLine());
+            
+            System.out.println("Habilitats:");
+            array[i].setHabilitats(ent.skip("[\r\n]*").nextLine());
+            
+            System.out.println("Transformacions:");
+            array[i].setTransformacions(ent.skip("[\r\n]*").nextLine());
+            
+            char esHome;
+            
+            do {
+                
+                System.out.println("És home o dona?(H/D):");
+                esHome = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0);
+                
+            } while (esHome != 'H' || esHome != 'D');
+            
+            if (esHome == 'H') {
+                array[i].setSexe(Personatges.Sexe.HOME);
+            } else {
+                array[i].setSexe(Personatges.Sexe.DONA);
+            }
+            array[i].setOmplit(true);
+            
+        } else {
+            System.out.println("\nNo en caben més, si vols, primer borra'n");
+        }
+    }
+    
 }
